9ce5d94242bbd8cfdfe325eb3fcb6697f8804b55
Stepan Putsiata <sstepanovich546@gmail.com>
2025-10-06 21:36:30 +0300

    configured work with Redux

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
+import { store } from '@/store';
 import { Stack } from 'expo-router';
-
+import { Provider } from 'react-redux';
 export default function RootLayout() {
+  return (
+    <Provider store={store}>
+      <AppStack />
+    </Provider>
+  );
+}
+
+function AppStack() {
   return <Stack />;
 }
