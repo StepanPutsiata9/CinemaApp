a91e0c55b894cf2201d06e8d1f55d35c304f8e52
Stepan Putsiata <sstepanovich546@gmail.com>
2025-11-20 22:27:02 +0300

    added unbooking ticket full

--- features/tickets/hooks/useTicketsList.tsx
+++ features/tickets/hooks/useTicketsList.tsx
@@ -1,7 +1,7 @@
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { useCallback } from 'react';
+import { Alert } from 'react-native';
 import { deleteTicketItem, getAllTickets, setTicketsError } from '../store/tickets.slice';
-
 export const useTicketsList = () => {
   const { ticketsError, ticketsList, ticketsLoading } = useAppSelector(state => state.ticketsList);
   const dispatch = useAppDispatch();
@@ -12,11 +12,32 @@ export const useTicketsList = () => {
       dispatch(setTicketsError(error instanceof Error ? error.message : 'Unknown error'));
     }
   }, [dispatch]);
+
   const handleDeleteTicket = async (ticketId: number) => {
+    Alert.alert(
+      'Отмена брони',
+      'Вы уверены, что хотите отменить бронь? Это действие невозможно отменить.',
+      [
+        {
+          text: 'Нет',
+          style: 'cancel',
+        },
+        {
+          text: 'Да',
+          style: 'destructive',
+          onPress: () => confirmDelete(ticketId),
+        },
+      ]
+    );
+  };
+
+  const confirmDelete = async (ticketId: number) => {
     try {
       await dispatch(deleteTicketItem(ticketId)).unwrap();
+      Alert.alert('Успех', 'Бронь успешно отменена');
     } catch (error) {
       console.error('Ошибка при удалении:', error);
+      Alert.alert('Ошибка', 'Произошла ошибка при отмене брони. Пожалуйста, попробуйте еще раз.');
     }
   };
   return {
